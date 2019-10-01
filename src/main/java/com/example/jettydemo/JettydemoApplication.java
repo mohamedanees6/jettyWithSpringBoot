ec11db5f3fbe7cb467910515b4feed8faaed391d
Mohamed Anees <mohamed.anees@cleartrip.com>
2019-10-01 22:50:53 +0530

    Initial commit

---
+++ src/main/java/com/example/jettydemo/JettydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JettydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JettydemoApplication.class, args);
+	}
+
+}

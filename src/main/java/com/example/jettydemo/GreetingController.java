994aad4ef9b7f1f3d93b20b4b112b204327babcf
Mohamed Anees <mohamed.anees@cleartrip.com>
2019-10-01 23:02:52 +0530

    Adding Greeting controller

--- src/main/java/com/example/jettydemo/GreetingController.java
+++ src/main/java/com/example/jettydemo/GreetingController.java
@@ -12,6 +12,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    //Sample Request http://localhost:8080/greeting?name=Anees
+    //Sample Response {"id":2,"content":"Hello, Anees!"}
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
